baee2ec935984b0e644675dab78e7aaf73c99a22
troennes <troennes@example.com>
2022-11-12 12:57:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Yearly yields
+
+Showcase your yearly yields from farming or gardening
+
+## Features
+
+- Works offline, without a webserver
+- No installation required
+- 90 icons included
+- Add your own images
+- Easy to modify
+- Easy to extend
+
+## How to use
+
+- Add your yields data to data.js
+  - name: what is it (title)
+  - description: subspecies or other description (subtitle)
+  - yield: amount and unit
+  - type: can be "fruit", "vegetable" or "product". Berries are fruits
+  - icon: filename in icons folder without .png (./icons/**fruit-apple**.png)
+  - img: (optional) filename in img folder without .jpg (./img/**apple-tree-1**.jpg)
+
+Example
+
+```js
+{
+  name: "Apple",
+  description: "Pink Lady",
+  yield: "2.8kg",
+  type: "fruit",
+  icon: "fruit-apple",
+  img: "apple-tree-1"
+},
+```
+
+How to find icons that fit in:
+
+1. Go to flaticon.com
+2. Search using the "Lineal Color" filter
+3. Download as 128x128px PNG file
+4. Store it in the icons folder
---
+++ data.js
@@ -0,0 +1,129 @@
+const data = 
+[
+// ----------------------------------------------------
+// fruit and berries example - replace with your own
+// ----------------------------------------------------
+  {
+    name: "Apple",
+    description: "Pink Lady",
+    yield: "2.8kg",
+    type: "fruit",
+    icon: "fruit-apple",
+    img: "apple-tree-1"
+  },
+  {
+    name: "Plum",
+    description: "Opal",
+    yield: "1.2kg",
+    type: "fruit",
+    icon: "stonefruit-plum",
+    img: ""
+  },
+  {
+    name: "Cucumber",
+    description: "Tasty Jade",
+    yield: "900g",
+    type: "fruit",
+    icon: "gourd-cucumber",
+    img: ""
+  },
+  {
+    name: "Tomato",
+    description: "Plum tomato",
+    yield: "600g",
+    type: "fruit",
+    icon: "tomato",
+    img: ""
+  },
+  {
+    name: "Strawberry",
+    description: "",
+    yield: "560g",
+    type: "fruit",
+    icon: "berry-strawberry",
+    img: ""
+  },
+  {
+    name: "Chilli",
+    description: "Carolina Reapers, 80x3g each",
+    yield: "240g",
+    type: "fruit",
+    icon: "chilli",
+    img: ""
+  },
+
+// -------------------------------------------------------------
+// vegetables, salads and herbs example - replace with your own
+// -------------------------------------------------------------
+  
+  {
+    name: "Potato",
+    description: "It was a very good year",
+    yield: "2.6kg",
+    type: "vegetable",
+    icon: "potato",
+    img: ""
+  },
+  {
+    name: "Carrot",
+    description: "Cosmic purple",
+    yield: "1.3kg",
+    type: "vegetable",
+    icon: "root-carrot",
+    img: ""
+  },
+  {
+    name: "Cabbage",
+    description: "Heads of 500g",
+    yield: "12",
+    type: "vegetable",
+    icon: "salad-cabbage",
+    img: ""
+  },
+  {
+    name: "Cauliflower",
+    description: "Heads of 400g",
+    yield: "7",
+    type: "vegetable",
+    icon: "vegetable-cauliflower",
+    img: ""
+  },
+  {
+    name: "Basil",
+    description: "",
+    yield: "50g",
+    type: "vegetable",
+    icon: "herb-basil",
+    img: ""
+  },
+  {
+    name: "Coriander",
+    description: "",
+    yield: "50g",
+    type: "vegetable",
+    icon: "herb-coriander",
+    img: ""
+  },
+
+// -------------------------------------------------------------
+// products example - replace with your own
+// -------------------------------------------------------------
+
+  {
+    name: "Strawberry jam",
+    description: "Jars of 200g",
+    yield: "4",
+    type: "product",
+    icon: "jar",
+    img: ""
+  },
+  {
+    name: "Hot sauce",
+    description: "Fermented chilli, jars of 200g",
+    yield: "3",
+    type: "product",
+    icon: "sauce",
+    img: ""
+  },  
+  
+];
